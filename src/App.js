170bf82efcb6eac8ce0cd2caf5466b6986f5bbe2
Firmansyah-13 <firman130305@gmail.com>
2023-12-02 17:21:22 +0700

    menambahkan Navbar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
+import Navbar from './components/Navbar.js';
 
 function App() {
   return (
     <>
-      
+      <Navbar />
     </>
   )
 }
+
 
 export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+function Navbar(){
+    return (
+      <>
+      <nav className='navbar'>
+        <div className='navbar-brand'>Vici Code</div>
+        <div className='nav-item'>About</div>
+        <div className='nav-item'>Blog</div>
+        <div className='nav-item'>Project</div>
+        <div className='nav-item'>Contact</div>
+      </nav>
+      </>
+    )
+  }
+
+  export default Navbar;
